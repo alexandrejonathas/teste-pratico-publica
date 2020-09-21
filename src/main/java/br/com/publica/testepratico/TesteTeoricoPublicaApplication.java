38902153125af6492b6bc90069ccbb83223fb4c0
Jonathas Lima <alexandrejonathas@gmail.com>
2020-09-21 10:18:15 -0300

    Primeiro commit

---
+++ src/main/java/br/com/publica/testepratico/TesteTeoricoPublicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.publica.testepratico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTeoricoPublicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTeoricoPublicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/publica/testepratico/TesteTeoricoPublicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.publica.testepratico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteTeoricoPublicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
